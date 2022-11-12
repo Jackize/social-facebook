2a97e8dc75dc914ccdb40f1fdb0e48562e473ac0
Jackize <nguyenthanhhao2042002@Gmail.com>
2022-11-12 11:28:15 +0700

    left bar

--- client/src/components/leftbar/LeftBar.jsx
+++ client/src/components/leftbar/LeftBar.jsx
@@ -1,15 +1,58 @@
 import React from 'react';
-import { Box } from '@mui/material';
-
+import {
+    Box,
+    Divider,
+    FormControlLabel,
+    FormGroup,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+} from '@mui/material';
+import { Home, OndemandVideo, Store } from '@mui/icons-material';
+import { MaterialUISwitch } from './leftBar.style';
 const LeftBar = () => {
     return (
-        <Box
-            bgcolor="yellow"
-            flex={1}
-            p={2}
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-        >
-            LeftBar
+        <Box flex={1} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Box width="70%">
+                <List>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <Home />
+                            </ListItemIcon>
+                            <ListItemText primary="Home" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <OndemandVideo />
+                            </ListItemIcon>
+                            <ListItemText primary="Watch" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <Store />
+                            </ListItemIcon>
+                            <ListItemText primary="Store" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+                <Divider />
+                <FormGroup>
+                    <FormControlLabel
+                        control={
+                            <MaterialUISwitch sx={{ m: 1 }} defaultChecked />
+                        }
+                        label="Dark mode"
+                        sx={{ ml: 2 }}
+                    />
+                </FormGroup>
+            </Box>
         </Box>
     );
 };
