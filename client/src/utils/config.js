8afa9e02685315c2dca6f5ece538dc3d254d1a62
Jackize <nguyenthanhhao2042002@Gmail.com>
2023-03-05 18:39:37 +0700

    update config to deploy

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,6 +1,6 @@
-export const URL_BE = "/api/";
+export const URL_BE = "https://nth-social-api.fly.dev/api/";
 export const CLOUD_NAME = "dpbhp5ury";
 export const CLOUD_PRESET = "ml_default";
 export const CLOUD_API_KEY = "168921146788365";
 export const CLOUD_API_SECRET = "xyGefjjzHufGEcQDwyyjNUrR1tk";
-export const SOCKET_SERVER = "ws://localhost:8080"
+export const SOCKET_SERVER = "ws://nth-social-api.fly.dev/"
