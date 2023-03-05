5a0ce2adf238e2f4e52182152699aa702929cc21
Jackize <nguyenthanhhao2042002@Gmail.com>
2023-03-05 18:25:02 +0700

    addconfig

---
+++ client/src/utils/config.js
@@ -0,0 +1,6 @@
+export const URL_BE = "/api/";
+export const CLOUD_NAME = "dpbhp5ury";
+export const CLOUD_PRESET = "ml_default";
+export const CLOUD_API_KEY = "168921146788365";
+export const CLOUD_API_SECRET = "xyGefjjzHufGEcQDwyyjNUrR1tk";
+export const SOCKET_SERVER = "ws://localhost:8080"
