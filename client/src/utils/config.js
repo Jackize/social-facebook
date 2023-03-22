5db019c95f9e78479e4d27ebaafe96a8823061b2
Jackize <nguyenthanhhao2042002@Gmail.com>
2023-03-22 17:15:20 +0700

    add .env

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,8 +1,6 @@
-export const CLOUD_NAME = "dpbhp5ury";
-export const CLOUD_PRESET = "ml_default";
-export const CLOUD_API_KEY = "168921146788365";
-export const CLOUD_API_SECRET = "xyGefjjzHufGEcQDwyyjNUrR1tk";
-// export const URL_BE = "http://localhost:8080/api/";
-// export const SOCKET_SERVER = "http://localhost:8080";
-export const URL_BE = "https://nth-social-api.fly.dev/api/";
-export const SOCKET_SERVER = "wss://nth-social-api.fly.dev";
+export const CLOUD_NAME = process.env.REACT_APP_CLOUD_NAME;
+export const CLOUD_PRESET = process.env.REACT_APP_CLOUD_PRESET;
+export const CLOUD_API_KEY = process.env.REACT_APP_CLOUD_API_KEY;
+export const CLOUD_API_SECRET = process.env.REACT_APP_CLOUD_API_SECRET;
+export const URL_BE = process.env.REACT_APP_BASE_URL;
+export const SOCKET_SERVER = process.env.REACT_APP_SOCKET_SERVER;
