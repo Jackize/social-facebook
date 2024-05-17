937bdc49b2d6d0fa34aa495508bff48277ddd5c3
Jackize <nguyenthanhhao2042002@Gmail.com>
2024-05-18 00:08:46 +0700

    Fix redis host and env Expire JWT

--- api/src/utils/redis.js
+++ api/src/utils/redis.js
@@ -1,7 +1,7 @@
 const { createClient } = require("redis");
 
 let client = createClient({
-  url: "redis://localhost:6379",
+  url: "redis://redis:6379",
 });
 
 const connectToRedis = async () => {
